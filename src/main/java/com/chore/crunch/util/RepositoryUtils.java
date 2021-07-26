f8d62f73f2f9ae752b645d4b5ead55ddc230a442
Ajay Sagar N <ajaysagarn@gmail.com>
2021-07-26 18:34:00 -0500

    add repository utils

---
+++ src/main/java/com/chore/crunch/util/RepositoryUtils.java
@@ -0,0 +1,18 @@
+package com.chore.crunch.util;
+
+public class RepositoryUtils {
+
+    public static boolean isNodePathNodeId(String nodePath){
+        if(nodePath.contains("/")) return false;
+        return true;
+    }
+
+    public static String trimSlashes(String nodePath){
+        if(nodePath.endsWith("/")){
+            nodePath = nodePath.substring(0, nodePath.length() - 1);
+        }
+
+        return nodePath;
+    }
+
+}
